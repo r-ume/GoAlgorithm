62ca912be7752264dcd6c1538492061179d2a425
r-ume <baymax.blackandwhite@gmail.com>
2017-12-30 17:44:25 +0900

    added statusChannel loop

--- review/goroutines2.go
+++ review/goroutines2.go
@@ -4,19 +4,20 @@ import(
   "fmt"
   "log"
   "net/http"
-  "sync"
+  // "sync"
 )
 
 func main(){
 
-  wait := new(sync.WaitGroup)
+  // wait := new(sync.WaitGroup)
   urls := []string{
     "http://example.com",
     "http://example.net",
     "http://example.org",
   }
+  statusChannel := make(chan string)
 
-  Without GoRoutine
+  // Without GoRoutine
   for _, url := range urls{
     response, err := http.Get(url)
     if err != nil{
@@ -28,9 +29,10 @@ func main(){
 
   // With GoRoutine
   for _, url := range urls {
+
     // waitGroupに追加
-    wait.Add(1)
-    
+    // wait.Add(1)
+
     // 取得処理をゴルーチンで実行する
     go func(url string) {
       res, err := http.Get(url)
@@ -38,11 +40,17 @@ func main(){
           log.Fatal(err)
         }
       defer res.Body.Close()
+      statusChannel <- res.Status
       fmt.Println(url, res.Status)
-      wait.Done()
+      // wait.Done()
     }(url)
   }
 
-  wait.Wait()
+  // ゴルーチンの中でstatusChanに値が書き込まれるまで，main()の中では値を読み出すことができません。
+  // この場合，main()内ではstatusChanの読み出しが3回完了するまで処理がブロックされるため，waitGroupのような待ち合わせ処理は必要がない。
+
+  for i := 0; i < len(urls); i++ {
+    fmt.Println(<-statusChannel)
+  }
 }
 
