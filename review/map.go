7a8e12aac0bde7c422a23259e0bdc3574455ab2d
r-ume <baymax.blackandwhite@gmail.com>
2018-01-25 22:29:27 +0900

    added keys, values, and indexes func in map.go.

--- review/map.go
+++ review/map.go
@@ -4,6 +4,43 @@ import "fmt"
 
 // Go langでは、連想配列のことをmapと呼ぶ
 func main() {
-    m := map[string]int{"apple": 150, "banana": 300, "lemon": 300}
-    fmt.Println(m) // => "map[apple:150 banana:300 lemon:300]"
+  m := map[string]int{"apple": 150, "banana": 300, "lemon": 300}
+  fmt.Println(m) // => "map[apple:150 banana:300 lemon:300]"
+
+  fmt.Println(keys(m))
+  fmt.Println(values(m))
+  fmt.Println(to_a(m))
+
+  keys := []string{"apple", "lemon"}
+  fmt.Println(indexes(m, keys))
 }
+
+func keys(m map[string]int) []string{
+  ks := []string{}
+
+  for k, _ := range m {
+    ks = append(ks, k)
+  }
+
+  return ks
+}
+
+func values(m map[string]int) []int {
+  vs := []int{}
+
+  for _, v := range m {
+    vs = append(vs, v)
+  }
+
+  return vs
+}
+
+func indexes(m map[string]int, keys []string) []int{
+  vs := []int{}
+  for _, k = range keys{
+    vs = append(vs, m[k])
+  }
+
+  return vs
+}
+
