f8d02ca7c0ae09be63231451e27e66aaf149c1f2
r-ume <baymax.blackandwhite@gmail.com>
2018-02-16 15:57:51 +0900

    added a migration file for post table

---
+++ goose/db/migrations/20180216155152_CreatePosts.sql
@@ -0,0 +1,12 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE `posts` (
+  `post_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `author`  VARCHAR(255) NOT NULL,
+  `text`    TEXT NOT NULL
+) ENGINE = InnoDB;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE `posts`;
