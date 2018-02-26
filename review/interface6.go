10ed74e0584e491cbf95ac5a4cf1386a4fc95166
r-ume <baymax.blackandwhite@gmail.com>
2018-02-26 13:31:55 +0900

    added interface6.go

---
+++ review/interface6.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Cry()
+}
+
+type Dog struct{}
+
+func (d Dog) Cry() {
+	fmt.Println("わんわん")
+}
+
+type Cat struct{}
+
+func (c Cat) Cry() {
+	fmt.Println("ニャーニャー")
+}
+
+func MakeSomeoneCry(someone interface{}) {
+	fmt.Println("鳴け！")
+	a, ok := someone.(Animal)
+	if !ok {
+		fmt.Println("動物では無いので鳴けません。")
+		return
+	}
+	a.Cry()
+}
+
+func main() {
+	dog := Dog{}
+	cat := Cat{}
+
+	MakeSomeoneCry(dog)
+	MakeSomeoneCry(cat)
+}
